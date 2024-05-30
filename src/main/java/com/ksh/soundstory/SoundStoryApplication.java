9671cf70df3c0a93cc19262b35d0f31747658852
sunghwan-d2 <ghktkf789@gmail.com>
2024-05-30 15:42:40 +0900

    feat : first commit - Sound Story html, scss

---
+++ src/main/java/com/ksh/soundstory/SoundStoryApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.soundstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundStoryApplication.class, args);
+	}
+
+}

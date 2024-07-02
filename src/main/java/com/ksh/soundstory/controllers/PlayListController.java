0aac4bc92168059a76290f732c01e56d2cb624fd
sunghwan-d2 <ghktkf789@gmail.com>
2024-07-02 18:39:55 +0900

    feat : 7.2 Commit - artist db 추가 후 조회되는지 확인

---
+++ src/main/java/com/ksh/soundstory/controllers/PlayListController.java
@@ -0,0 +1,11 @@
+package com.ksh.soundstory.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("playlist")
+public class PlayListController {
+
+}
